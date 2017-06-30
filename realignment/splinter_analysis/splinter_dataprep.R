595d101fd3729ad75fe62bf57c1e3ff7be92b31f
dbowden <dbowden88@gmail.com>
2017-06-30 11:06:14 -0500

    finished realignment chapter

--- realignment/splinter_analysis/splinter_dataprep.R
+++ realignment/splinter_analysis/splinter_dataprep.R
@@ -2,6 +2,10 @@
 library(dplyr)
 library(foreign)
 library(tidyr)
+library(readxl)
+library(readr)
+library(WDI)
+library(countrycode)
 
 load("master_rebel_yearly.Rdata")
 
@@ -53,9 +57,9 @@ group.years <- group.years %>%
 group.years$time <- group.years$Year - group.years$start
 
 
-# 4. Merge in some country-level covariates -----
+#### Merge in some country-level covariates ####
 
-#HPS
+# 4. HPS ------
 hrp <- read.csv("~/Dropbox/Dissertation/Document/entry_chapter/entry_analysis/HumanRightsProtectionScores_v2.04.csv")
 
 hrp <- rename(hrp, Year = YEAR, GWNoA = COW)
@@ -78,7 +82,7 @@ group.years$latentmean_diff <- group.years$latentmean - group.years$latentmean_l
 
 group.years$latentmean_diff2 <- group.years$latentmean_lag - group.years$latentmean_lag2
 
-#Ethfrac
+# 5. Ethfrac ------
 fearon <- read.dta("~/Dropbox/Dissertation/Document/entry_chapter/entry_analysis/repdata.dta")
 
 fearon <- select(fearon, GWNoA = ccode, Year = year, mtnest, Oil, ethfrac, relfrac)
@@ -96,3 +100,65 @@ group.years <- group.years %>%
   fill(ethfrac, mtnest, .direction = "up")
 
 
+# 6. Controls: Polity -----
+polity <- read_excel("~/Dropbox/Dissertation/Document/entry_chapter/entry_analysis/p4v2015.xls")
+
+polity <- select(polity, GWNoA = ccode, Year = year, polity2)
+
+group.years <- left_join(group.years, polity)
+
+rm(polity)
+
+# 7. GDP and pop ------
+gdp <- read_delim("~/Dropbox/Dissertation/Document/entry_chapter/entry_analysis/expgdpv6.0/gdpv6.txt", delim="\t")
+
+gdp <- select(gdp, GWNoA = statenum, Year = year, pop, realgdp, rgdppc)
+
+group.years <- left_join(group.years, gdp)
+
+rm(gdp)
+
+
+# 8. Controls: Area ------
+area <- WDI(country="all", indicator="AG.LND.TOTL.K2", start=1960, end=2015)
+
+area$GWNoA <- countrycode(area$iso2c, "iso2c", "cown")
+
+area <- select(area, GWNoA, Year=year, area=AG.LND.TOTL.K2)
+
+group.years <- left_join(group.years, area)
+
+#fill pre-1960
+group.years <- group.years %>%
+  group_by(DyadId) %>%
+  fill(area, .direction=c("up"))
+
+rm(area)
+
+# 9. Resource sites --------
+loot <- read.csv("~/Dropbox/Dissertation/Document/entry_chapter/entry_analysis/all_resources_country.csv")
+
+loot <- rename(loot, GWNoA = COWCODE)
+
+loot$loot.dia <- ifelse(!is.na(loot$diamond.sites), 1, 0)
+loot$loot.gem <- ifelse(!is.na(loot$gem.sites), 1, 0)
+loot$loot.gold <- ifelse(!is.na(loot$gold.sites), 1, 0)
+loot$loot.oil <- ifelse(!is.na(loot$oil.sites), 1, 0)
+loot$loot.drugs <- ifelse(!is.na(loot$drug.sites), 1, 0)
+loot$lootable <- ifelse(loot$loot.dia==1 | loot$loot.gem==1 | loot$loot.gold==1 | loot$loot.oil==1 | loot$loot.drugs==1, 1, 0)
+
+loot <- select(loot, GWNoA, loot.dia, loot.gem, loot.gold, loot.oil, loot.drugs, lootable, tot.resource.sites)
+
+group.years <- left_join(group.years, loot)
+
+rm(loot)
+
+# 10. Prep some vars for survival analysis -----
+group.years$time1 <- group.years$time + 1
+
+group.years$stronger <- ifelse(group.years$rebstrength=="stronger" | group.years$rebstrength=="much stronger", 1, 0)
+group.years$multieth <- ifelse(group.years$tot_eth>1, 1, 0)
+
+# 11. Write data -------
+saveRDS(group.years, "survival_data.Rdata")
+write.csv(group.years, "survival_data.csv", row.names = F)
--- realignment/survival_models.R
+++ realignment/survival_models.R
@@ -7,16 +7,16 @@ group.years$multieth <- ifelse(group.years$tot_eth>1, 1, 0)
 
 S1 <- Surv(group.years$time, group.years$time1, group.years$death)
 
-m1 <- coxph(S1 ~ latentmean_diff + multieth + cluster(GWNoLoc), data=data.frame(group.years))
+m1 <- coxph(S1 ~ latentmean_diff + multieth + IntensityLevel + cluster(GWNoLoc), data=data.frame(group.years))
 summary(m1)
 
 texreg::screenreg(m1)
 
 
-m2 <- coxph(S1 ~ latentmean_diff + multieth + ethfrac + cluster(GWNoLoc), data=group.years)
+m2 <- coxph(S1 ~ latentmean_diff + multieth + IntensityLevel + ethfrac + cluster(GWNoLoc), data=group.years)
 summary(m2)
 
-m3 <- coxph(S1 ~ latentmean_diff + multieth + rebpresosts_bin + rebpolwing_bin + stronger + cluster(GWNoLoc), data=group.years)
+m3 <- coxph(S1 ~ latentmean_diff + multieth  + polity2 + rebpresosts_bin + rebpolwing_bin + stronger + IntensityLevel + cluster(GWNoLoc), data=group.years)
 summary(m3)
 
 fit <- survfit(Surv(time, time1, death) ~ (latentmean_diff < 0), data=group.years)
